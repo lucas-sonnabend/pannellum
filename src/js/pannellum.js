ff9830859ff6d2e269fbf06846648e24f8eebae1
Lucas Sonnabend <lucas@disperse.io>
2021-05-22 18:47:09 +0100

    add event that fires on every render

--- src/js/pannellum.js
+++ src/js/pannellum.js
@@ -1656,6 +1656,7 @@ function render() {
             compass.style.transform = 'rotate(' + (-config.yaw - config.northOffset) + 'deg)';
             compass.style.webkitTransform = 'rotate(' + (-config.yaw - config.northOffset) + 'deg)';
         }
+        fireEvent('renderFinished', {pitch: _this.getPitch(), yaw: _this.getYaw(), hfov: _this.getHfov()});
     }
 }
 
