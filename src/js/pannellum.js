11e34ae21197903f59d05a6e2ef105de0cfdd247
Lucas Sonnabend <lucas@disperse.io>
2021-05-26 17:35:25 +0100

    include sceneId in on render change event

--- src/js/pannellum.js
+++ src/js/pannellum.js
@@ -1656,7 +1656,7 @@ function render() {
             compass.style.transform = 'rotate(' + (-config.yaw - config.northOffset) + 'deg)';
             compass.style.webkitTransform = 'rotate(' + (-config.yaw - config.northOffset) + 'deg)';
         }
-        fireEvent('renderFinished', {pitch: _this.getPitch(), yaw: _this.getYaw(), hfov: _this.getHfov()});
+        fireEvent('renderFinished', {sceneId: config.scene, pitch: config.pitch, yaw: config.yaw, hfov: config.hfov});
     }
 }
 
